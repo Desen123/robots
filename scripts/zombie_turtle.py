a149ad38666dfbecc3873bb80c86d3c994959f07
Desen123 <gder-korepanov@yandex.ru>
2023-05-15 00:43:23 +0400

    try to spawn

--- scripts/zombie_turtle.py
+++ scripts/zombie_turtle.py
@@ -1,40 +1,55 @@
 import rospy
-from std_msgs.msg import String
+import math
+from geometry_msgs.msg import Twist
+from turtlesim.msg import Pose
 
 
-def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        rate.sleep()
+class Zombie:
+    def __init__(self):
+        self.x = 1
+        self.y = 1
+        self.phi = 0
+        self.lin_speed = rospy.get_param('~speed_linear', 1.0)
+        self.ang_speed = rospy.get_param('~speed_angular', 1.0)
 
+        rospy.loginfo('init done')
+        rospy.wait_for_service('spawn')
+        try: 
+            rospy.loginfo('spawning turtle')
+            spawner = rospy.ServiceProxy('spawn', turtlesim/Spawn)
+            spawner(self.x, self.y, self.phi, "")
+            # pub = rospy.Publisher('chatter', String, queue_size=10)
+            # rate = rospy.Rate(10) # 10hz
+            rospy.loginfo('listen')
+            self.listener()
+        except rospy.ServiceException as e:
+            print ('Service call fail: %s', e)
 
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
+    def go_to_goal(self, x_goal, y_goal):
+        velocity_message = Twist()
+        cmd_vel_topic='/turtle2/cmd_vel'
+        
+        distance = abs(math.sqrt(((x_goal - self.x) ** 2) + (y_goal - self.y) ** 2))
+        linear_speed = distance * self.lin_speed
 
-def listener():
+        desired_agle_goal = math.atan2(y_goal - self.y, x_goal - self.x)
+        angular_speed = (desired_agle_goal - self.phi) * self.ang_speed
 
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    rospy.init_node('listener', anonymous=True)
+        velocity_message.linear.x = linear_speed
+        velocity_message.angular.z = angular_speed
 
-    rospy.Subscriber('chatter', String, callback)
+    def callback(self, data):
+        rospy.loginfo(rospy.get_caller_id() + ' heard\n x: %s\n y: %s ', data.x, data.y)
+        # self.go_to_goal(data.x, data.y)
 
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
-  
+    def listener(self):
+        rospy.init_node('listener', anonymous=True)
+        rospy.Subscriber('turtle1/pose', Pose, self.callback)
+        rospy.spin()
 
 
 if __name__ == '__main__':
     try:
-        listener()
-        talker()
+        Zombie()
     except rospy.ROSInterruptException:
         pass
